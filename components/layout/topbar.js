1513e5cfc4fbe93beda854caec3175ee3a12073f
Luong Quang Manh <luongquangmanh85@gmail.com>
2018-12-23 16:13:43 +0700

    Improve UI

--- components/layout/topbar.js
+++ components/layout/topbar.js
@@ -12,10 +12,10 @@ export default class TopBar extends PureComponent {
 
   render() {
     return (
-      <nav className="navbar has-shadow is-light">
+      <nav className="navbar has-shadow is-white">
         <div className="navbar-brand">
           <Link prefetch href='/'><a className="navbar-item">
-            <h4 className='title is-4'>LQM</h4>
+            <h4 className='title is-4'>lqm</h4>
           </a></Link>
           <a className={`navbar-burger ${this.state.burgerActive ? 'is-active' : ''}`} onClick={this.toggleBurger}>
             <span></span>
@@ -24,13 +24,22 @@ export default class TopBar extends PureComponent {
           </a>
         </div>
         <div className={`navbar-menu ${this.state.burgerActive ? 'is-active' : ''}`}>
-          <div className="navbar-start">
+          {/* <div className="navbar-start">
             <Link prefetch href='/about'><a className="navbar-item">About</a></Link>
+          </div> */}
+          <div className="navbar-end">
+            <div className='navbar-item'>
+              <span className='quote'>&ldquo;Writing is the paiting of the voice.&rdquo;</span>
+              <span>&nbsp;&ndash; Voltaire</span>
+            </div>
           </div>
         </div>
         <style jsx>{`
           .title {
-            letter-spacing: 0.25rem;
+            font-family: 'Major Mono Display', monospace;
+          }
+          .quote {
+            font-family: 'Kalam', cursive;
           }
         `}</style>
       </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import IntroHero from '../components/intro-hero'
 import Layout from '../components/layout'
 import PostList from '../components/post/post-list'
 
 
 export default (props) => (
   <Layout>
-    <IntroHero />
     <section className='section columns is-centered'>
       <div className='column is-6'>
         <PostList />
