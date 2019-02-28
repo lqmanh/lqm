51e68059110868a6e502b84f5ba1f205ac1ec5e6
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-03-01 01:48:18 +0700

    Add the default image for PostBullet. Upgrade dependencies

--- components/post/post-bullet.js
+++ components/post/post-bullet.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 export default (props) => {
-  const { title, headerImage } = props.meta
+  const { title, headerImage = '/static/default.jpg' } = props.meta
   return (
     <article className='media'>
       <div className='media-left'>
