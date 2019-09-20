fe3cd22796d07d52099226c8d4bdd3c0441f47f4
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-09-21 01:53:53 +0700

    Fix all Next.js 9 issues. LQM is a hybrid web app now

--- components/post/post-cards.js
+++ components/post/post-cards.js
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import PostCard from './post-card'
 import { publicationDate } from '../../utils/sort-criteria'
-let { children: posts } = require('../../content/.dirstat.json')
 
 export default () => {
-  posts = posts.filter((post) => post.meta.published)
-  posts.sort(publicationDate)
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let { data: posts } = await axios.get('/api/posts')
+      posts = posts.filter((post) => post.meta.published)
+      posts.sort(publicationDate)
+      setPosts(posts)
+    }
+    fetchData()
+  }, [])
+
+  if (!posts.length) return null
   return (
     <>
       {posts.map((post, i) => (
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,36 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
-
+import axios from 'axios'
 import Layout from '../components/layout'
 import LeftSideBar from '../components/side-bar/left-side-bar'
 import PostCards from '../components/post/post-cards'
-const { children: posts } = require('../content/.dirstat.json')
 
-export default () => (
-  <>
-    <Head>
-      <title>LQM by Lương Quang Mạnh</title>
-    </Head>
-    <Layout left={<LeftSideBar posts={posts} />}>
-      <section className='section'>
-        <div className='columns is-centered'>
-          <div className='column is-12-tablet is-10-desktop is-8-widescreen'>
-            <PostCards posts={posts} />
+export default () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: posts } = await axios.get('/api/posts')
+      setPosts(posts)
+    }
+    fetchData()
+  }, [])
+
+  if (!posts.length) return null
+  return (
+    <>
+      <Head>
+        <title>LQM by Lương Quang Mạnh</title>
+      </Head>
+      <Layout left={<LeftSideBar posts={posts} />}>
+        <section className='section'>
+          <div className='columns is-centered'>
+            <div className='column is-12-tablet is-10-desktop is-8-widescreen'>
+              <PostCards posts={posts} />
+            </div>
           </div>
-        </div>
-      </section>
-    </Layout>
-  </>
-)
+        </section>
+      </Layout>
+    </>
+  )
+}
