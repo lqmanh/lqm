4dcc3bc8809ba99530f3a032028706ca2bd1a2db
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-09-21 02:05:08 +0700

    Fix all 3rd-party scripts

--- pages/_document.js
+++ pages/_document.js
@@ -13,14 +13,13 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='vi' prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#'>
-        <Head>
-          <script async src='https://www.googletagmanager.com/gtag/js?id=UA-133189175-1' />
-          <script dangerouslySetInnerHTML={analyticsMarkup} />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
-          <script async src='https://unpkg.com/ionicons@4.6.1/dist/ionicons.js' />
+          <script src='https://unpkg.com/ionicons@4.6.3/dist/ionicons.js' />
+          <script src='https://www.googletagmanager.com/gtag/js?id=UA-133189175-1' />
+          <script dangerouslySetInnerHTML={analyticsMarkup} />
         </body>
       </Html>
     )
