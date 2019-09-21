3a6edab1acac35cafaf4e3d56eca0e6b22a28789
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-09-21 17:17:17 +0700

    Make /api/* dynamic

--- content-builder.js
+++ content-builder.js
@@ -4,28 +4,6 @@ const { StatWriter } = require('directory-stat')
 const { StatCollector } = require('directory-stat/stat-collectors')
 const Converter = require('showdown').Converter
 const fm = require('front-matter')
-const axios = require('axios')
-
-const FLICKR_API_KEY = '3aedad13c4053365a085f050c12f07b7'
-const FLICKR_USER_ID = '184521961@N05'
-
-const getPhotoUrls = async (albumID) => {
-  const {
-    data: {
-      photoset: { photo }
-    }
-  } = await axios.get(
-    'https://www.flickr.com/services/rest/' +
-      '?method=flickr.photosets.getPhotos' +
-      '&format=json' +
-      '&nojsoncallback=1' +
-      '&extras=url_o' +
-      `&api_key=${FLICKR_API_KEY}` +
-      `&user_id=${FLICKR_USER_ID}` +
-      `&photoset_id=${albumID}`
-  )
-  return photo.map((p) => p.url_o)
-}
 
 const getBodyHtml = (body) => {
   const converter = new Converter({
@@ -58,16 +36,13 @@ class BlogPostMetaCollector extends StatCollector {
     const { attributes: meta, body } = fm(await fs.readFile(pathStr, { encoding: 'utf8' }))
 
     // side effect
-    let bodyHtml, photoUrls
-    if (meta.type === 'album') {
-      photoUrls = await getPhotoUrls(meta.albumID)
-      meta.headerImage = photoUrls[0]
-    } else {
+    let bodyHtml
+    if (!meta.type || meta.type === 'article') {
       bodyHtml = getBodyHtml(body)
     }
     const { dir, name } = path.parse(pathStr)
     try {
-      fs.writeFile(path.join(dir, `${name}.json`), JSON.stringify({ bodyHtml, photoUrls, ...meta }))
+      fs.writeFile(path.join(dir, `${name}.json`), JSON.stringify({ bodyHtml, ...meta }))
     } catch (err) {
       console.error('Error while converting .md to .json')
     }
