8d64389cbd57f95b0e15448d66d881a1c1d24120
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2019-11-30 12:57:52 +0700

    Improve performance

--- components/post/post-cards.js
+++ components/post/post-cards.js
@@ -1,16 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import PostCard from './post-card'
-import { publicationDate } from '../../utils/sort-criteria'
 
 export default () => {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
-      let { data: posts } = await axios.get('/api/posts')
-      posts = posts.filter((post) => post.meta.published)
-      posts.sort(publicationDate)
+      const { data: posts } = await axios.get('/api/posts?sortBy=publicationDate')
       setPosts(posts)
     }
     fetchData()
--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,8 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
+import { published } from '../../utils/filter-criteria'
+import { publicationDate, lastUpdatedDate } from '../../utils/sort-criteria'
 
 const FLICKR_API_KEY = '3aedad13c4053365a085f050c12f07b7'
 const FLICKR_USER_ID = '184521961@N05'
+
+const { children: posts } = require('../../content/.dirstat.json')
 
 const getPrimaryPhotoUrl = async (albumID: string): Promise<string> => {
   const {
@@ -25,9 +29,11 @@ const getPrimaryPhotoUrl = async (albumID: string): Promise<string> => {
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { children: posts } = require('../../content/.dirstat.json')
+  const filtered = posts.filter(published)
+  if (req.query.sortBy == 'lastUpdatedDate') filtered.sort(lastUpdatedDate)
+  else filtered.sort(publicationDate)
 
-  for (const post of posts) {
+  for (const post of filtered) {
     const { meta } = post
     if (meta.type === 'album') {
       meta.headerImage = await getPrimaryPhotoUrl(meta.albumID)
